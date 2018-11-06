b54bdbff36522cfca4a16b8ca207f05182acd3a7
asdasd <asdasd@LAPTOP-KGUDAS9O>
2018-11-05 21:50:07 -0300

    Master compilado y funcionando

--- misOfertas/Consumidor/ErrorEvaluar.aspx.cs
+++ misOfertas/Consumidor/ErrorEvaluar.aspx.cs
@@ -1,5 +1,4 @@
-﻿using misOfertas.BLL;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,13 +7,11 @@ using System.Web.UI.WebControls;
 
 namespace misOfertas.Consumidor
 {
-    public partial class Evaluar : System.Web.UI.Page
+    public partial class ErrorEvaluar : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
 
-            //Valoracion valoracion = new Valoracion();
-            //oferta.PRECIO_NORMAL =
         }
     }
 }
